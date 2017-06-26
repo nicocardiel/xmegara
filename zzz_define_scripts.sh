9d846581d2c4f97a742c5438982959fd6798ac53
Nicolás Cardiel <cardiel@ucm.es>
2017-06-26 19:18:41 +0100

    Update script with alias declarations

--- zzz_define_scripts.sh
+++ zzz_define_scripts.sh
@@ -5,8 +5,5 @@ alias MEGARA_qla='~/s/megara-qla-1.3.0/bin/megara-qla'
 alias MEGARA_overplot_traces=\
 'python ~/s/xmegara/overplot_traces.py'
 
-alias MEGARA_shift_master_traces=\
-'python ~/s/xmegara/shift_master_traces.py'
-
 # to visualize RSS images in 2D (in LCB mode)  
 alias MEGARA_visualization='python -m megaradrp.visualization'
