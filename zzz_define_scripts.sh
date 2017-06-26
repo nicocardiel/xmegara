c92bdada1467d2670751b3284225dbebf705a49d
Nicolás Cardiel <cardiel@ucm.es>
2017-06-26 12:53:00 +0100

    Include first scripts.

---
+++ zzz_define_scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+alias MEGARA_qla='~/s/megara-qla-1.3.0/bin/megara-qla'
+
+alias MEGARA_overplot_traces=\
+'python ~/s/python/MEGARA/xmegara/overplot_traces.py'
+
+alias MEGARA_shift_master_traces=\
+'python ~/s/python/MEGARA/xmegara/shift_master_traces.py'
+
+# to visualize RSS images in 2D (in LCB mode)  
+alias MEGARA_visualization='python -m megaradrp.visualization'
