40422867deae0e2f414dd62c231fcd088842f1aa
nicocardiel <cardiel@ucm.es>
2017-07-28 23:36:05 +0000

    Display boxes in addition to fibid.

--- overplot_traces.py
+++ overplot_traces.py
@@ -13,8 +13,45 @@ from numina.array.display.ximshow import ximshow_file
 from numina.array.display.pause_debugplot import pause_debugplot
 
 
+def assign_boxes_to_fibers(insmode):
+    """Read boxes in configuration file and assign values to fibid
+
+    Parameters
+    ----------
+    insmode : string
+        Value of the INSMODE keyword: 'LCB' or 'MOS'.
+
+    Returns
+    -------
+    fibid_with_box : list of strings
+        List with string label that contains both the fibid and the
+        box name.
+
+    """
+    sdum = pkgutil.get_data(
+        'megaradrp.instrument.configs',
+        'component-86a6e968-8d3d-456f-89f8-09ff0c7f7c57.json'
+    )
+    dictdum = json.loads(sdum)
+    pseudo_slit_config = \
+        dictdum['configurations']['boxes']['values'][insmode]
+
+    fibid_with_box = []
+    n1 = 1
+    for dumbox in pseudo_slit_config:
+        nfibers = dumbox['nfibers']
+        name = dumbox['name']
+        n2 = n1 + nfibers
+        fibid_with_box += \
+            ["{}  [{}]".format(val1, val2)
+             for val1, val2 in zip(range(n1, n2), [name] * nfibers)]
+        n1 = n2
+
+    return fibid_with_box
+
+
 def plot_trace(ax, coeff, xmin, xmax, ix_offset,
-               rawimage, insmode, fibids, fibid, colour):
+               rawimage, fibids, fiblabel, colour):
     if xmin == xmax == 0:
         num = 4096
         xp = np.linspace(start=1, stop=4096, num=num)
@@ -32,10 +69,10 @@ def plot_trace(ax, coeff, xmin, xmax, ix_offset,
             xmidpoint = 2048
         else:
             xmidpoint = (xmin+xmax)/2
-        #
-        ax.text(xmidpoint, yp[int(num / 2)], str(fibid), fontsize=6,
+        ax.text(xmidpoint, yp[int(num / 2)], fiblabel, fontsize=6,
                 bbox=dict(boxstyle="round,pad=0.1", fc="white", ec="grey", ),
-                color=colour, fontweight='bold', backgroundcolor='white')
+                color=colour, fontweight='bold', backgroundcolor='white',
+                ha='center')
 
 
 def main(args=None):
@@ -76,7 +113,8 @@ def main(args=None):
                         help="tuple of FITS keywords.format: " +
                              "key1,key2,...keyn.'format'")
     parser.add_argument("--geometry",
-                        help="tuple x,y,dx,dy")
+                        help="tuple x,y,dx,dy",
+                        default="0,0,640,480")
     parser.add_argument("--pdffile",
                         help="ouput PDF file name",
                         type=argparse.FileType('w'))
@@ -90,9 +128,7 @@ def main(args=None):
     else:
         import matplotlib
         matplotlib.use('Qt5Agg')
-        import matplotlib.pyplot as plt
         pdf = None
-
 
     ax = ximshow_file(args.fits_file.name,
                       args_cbar_orientation='vertical',
@@ -112,11 +148,13 @@ def main(args=None):
     # read and display traces from JSON file
     bigdict = json.loads(open(args.traces_file.name).read())
     insmode = bigdict['tags']['insmode']
+    fibid_with_box = assign_boxes_to_fibers(insmode)
     global_offset = bigdict['global_offset']
     pol_global_offset = np.polynomial.Polynomial(global_offset)
     ref_column = bigdict['ref_column']
     for fiberdict in bigdict['contents']:
         fibid = fiberdict['fibid']
+        fiblabel = fibid_with_box[fibid - 1]
         xmin = fiberdict['start']
         xmax = fiberdict['stop']
         coeff = np.array(fiberdict['fitparms'])
@@ -130,9 +168,9 @@ def main(args=None):
             bigdict['contents'][fibid-1]['fitparms'] = coeff.tolist()
             if args.extrapolate:
                 plot_trace(ax, coeff, 0, 0, ix_offset, args.rawimage,
-                           insmode, False, fibid, colour='grey')
+                           False, fiblabel, colour='grey')
             plot_trace(ax, coeff, xmin, xmax, ix_offset, args.rawimage,
-                       insmode, args.fibids, fibid, colour='blue')
+                       args.fibids, fiblabel, colour='blue')
         else:
             print('Warning ---> Missing fiber:', fibid)
 
@@ -143,6 +181,7 @@ def main(args=None):
         if 'badfibers' in healdict.keys():
             for badfiberdict in healdict['badfibers']:
                 fibid = badfiberdict['fibid']
+                fiblabel = fibid_with_box[fibid - 1]
                 print("(healing) fibid: ", fibid)
                 if badfiberdict['method'] == 'interpolation2':
                     fraction = badfiberdict['fraction']
@@ -150,14 +189,16 @@ def main(args=None):
                     tmpf1 = bigdict['contents'][nf1-1]
                     tmpf2 = bigdict['contents'][nf2-1]
                     if nf1 != tmpf1['fibid'] or nf2 != tmpf2['fibid']:
-                        raise ValueError("Unexpected fiber numbers in neighbours")
+                        raise ValueError(
+                            "Unexpected fiber numbers in neighbours"
+                        )
                     coefff1 = np.array(tmpf1['fitparms'])
                     coefff2 = np.array(tmpf2['fitparms'])
                     xmin = np.min([tmpf1['start'], tmpf2['start']])
                     xmax = np.min([tmpf1['stop'], tmpf2['stop']])
                     coeff = coefff1 + fraction * (coefff2 - coefff1)
                     plot_trace(ax, coeff, xmin, xmax, ix_offset,
-                               args.rawimage, insmode, args.fibids, fibid,
+                               args.rawimage, args.fibids, fiblabel,
                                colour='green')
                     # update values in bigdict (JSON structure)
                     bigdict['contents'][fibid - 1]['start'] = xmin
@@ -172,6 +213,7 @@ def main(args=None):
         if 'extrapolations_single' in healdict.keys():
             for extrapoldict in healdict['extrapolations_single']:
                 fibid = extrapoldict['fibid']
+                fiblabel = fibid_with_box[fibid - 1]
                 print("(extrapolating) fibid: ", fibid)
                 # update values in bigdict (JSON structure)
                 xmin = extrapoldict['start']
@@ -183,16 +225,15 @@ def main(args=None):
                 coeff = np.array(bigdict['contents'][fibid - 1]['fitparms'])
                 if xmin < xmin_orig:
                     plot_trace(ax, coeff, xmin, xmin_orig, ix_offset,
-                               args.rawimage, insmode, False, fibid,
-                               colour='green')
+                               args.rawimage, False, fiblabel, colour='green')
                 if xmax_orig < xmax:
                     plot_trace(ax, coeff, xmax_orig, xmax, ix_offset,
-                               args.rawimage, insmode, False, fibid,
-                               colour='green')
+                               args.rawimage, False, fiblabel, colour='green')
 
         if 'extrapolation_single_fixed_points' in healdict.keys():
             for extrapoldict in healdict['extrapolation_single_fixed_points']:
                 fibid = extrapoldict['fibid']
+                fiblabel = fibid_with_box[fibid - 1]
                 print("(extrapolation+fixed): ", fibid)
                 start_reuse = extrapoldict['start_reuse']
                 stop_reuse = extrapoldict['stop_reuse']
@@ -205,8 +246,9 @@ def main(args=None):
                 poly = np.polynomial.Polynomial(coeff)
                 yfit = poly(xfit)
                 for fixedpoint in extrapoldict['fixed_points']:
-                    # assume x, y coordinates in JSON file are given in image coordinates,
-                    # starting at (1,1) in the lower left corner
+                    # assume x, y coordinates in JSON file are given in
+                    # image coordinates, starting at (1,1) in the lower
+                    # left corner
                     xdum = fixedpoint['x'] - 1  # use np.array coordinates
                     ydum = fixedpoint['y'] - 1  # use np.array coordinates
                     xfit = np.concatenate((xfit, np.array([xdum])))
@@ -215,11 +257,11 @@ def main(args=None):
                 coeff = poly.coef
                 if start < start_reuse:
                     plot_trace(ax, coeff, start, start_reuse, ix_offset,
-                               args.rawimage, insmode, args.fibids, fibid,
+                               args.rawimage, args.fibids, fiblabel,
                                colour='green')
                 if stop_reuse < stop:
                     plot_trace(ax, coeff, stop_reuse, stop, ix_offset,
-                               args.rawimage, insmode, args.fibids, fibid,
+                               args.rawimage, args.fibids, fiblabel,
                                colour='green')
                 bigdict['contents'][fibid - 1]['start'] = extrapoldict['start']
                 bigdict['contents'][fibid - 1]['stop'] = extrapoldict['stop']
@@ -230,6 +272,7 @@ def main(args=None):
                 fibid_ini = extrapoldict['fibid_ini']
                 fibid_end = extrapoldict['fibid_end']
                 for fibid in range(fibid_ini, fibid_end + 1):
+                    fiblabel = fibid_with_box[fibid - 1]
                     print("(extrapolating) fibid: ", fibid)
                     # update values in bigdict (JSON structure)
                     xmin = extrapoldict['start']
@@ -238,14 +281,15 @@ def main(args=None):
                     xmax_orig = bigdict['contents'][fibid - 1]['stop']
                     bigdict['contents'][fibid - 1]['start'] = xmin
                     bigdict['contents'][fibid - 1]['stop'] = xmax
-                    coeff = np.array(bigdict['contents'][fibid - 1]['fitparms'])
+                    coeff = \
+                        np.array(bigdict['contents'][fibid - 1]['fitparms'])
                     if xmin < xmin_orig:
                         plot_trace(ax, coeff, xmin, xmin_orig, ix_offset,
-                                   args.rawimage, insmode, False, fibid,
+                                   args.rawimage, False, fiblabel,
                                    colour='green')
                     if xmax_orig < xmax:
                         plot_trace(ax, coeff, xmax_orig, xmax, ix_offset,
-                                   args.rawimage, insmode, False, fibid,
+                                   args.rawimage, False, fiblabel,
                                    colour='green')
 
     # update trace map
@@ -261,7 +305,7 @@ def main(args=None):
         pdf.savefig()
         pdf.close()
     else:
-        pause_debugplot(12, pltshow=True)
+        pause_debugplot(12, pltshow=True, tight_layout=True)
 
 
 if __name__ == "__main__":
