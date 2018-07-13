f266d7c61f5919daa2f7f5aef1805a66c2200919
Nicolás Cardiel <cardiel@ucm.es>
2018-07-13 11:00:54 +0200

    Update zzz_define_scripts.sh

--- zzz_define_scripts.sh
+++ zzz_define_scripts.sh
@@ -5,9 +5,6 @@ alias MEGARA_qla='~/s/megara-qla-1.5.0/bin/megara-qla'
 alias MEGARA_find_boxes='python ~/s/xmegara/find_boxes.py'
 # note: find_boxes.py is also found in megaradrp/tools
 
-alias MEGARA_overplot_traces='python ~/s/xmegara/overplot_traces.py'
-# note: overplot_traces.py is also found in megaradrp/tools
-
 alias MEGARA_traces2ds9='python ~/s/xmegara/traces2ds9.py'
 # this functionality has been incorporated in megaradrp; see:
 # megaradrp/products/tracemap.py,
